8f82a757d72f736d4df6953cbb28a554cc611e94
Collin Clark <crclark96@gmail.com>
2017-11-02 13:40:36 -0400

    add sender.py skeleton

---
+++ sender.py
@@ -0,0 +1,18 @@
+import socket
+
+num_packets = 20
+
+s_tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s_udp = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+receiver_ip = '127.0.0.1'
+receiver_port_tcp = 7777
+receiver_port_udp = 7778
+
+s_tcp.connect((receiver_ip, receiver_port_tcp))
+s_udp.connect((receiver_ip, receiver_port_udp))
+for i in range(num_packets):
+    s_tcp.sendall(i)
+    s_udp.sendall(i)
+
+print('done')
