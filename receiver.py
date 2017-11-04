34696692e790061915582e97f7517998f00acea6
Collin Clark <crclark96@gmail.com>
2017-11-04 18:41:46 -0400

    get shit done

--- receiver.py
+++ receiver.py
@@ -8,11 +8,13 @@ Created on Thu Nov  2 13:08:50 2017
 import socket
 import time
 
+packet_size = 2048
+
 s_tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s_tcp.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 s_udp = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-receiver_ip = '0.0.0.0'
+receiver_ip = '127.0.0.1'
 receiver_port_tcp = 7777
 receiver_port_udp = 7778
 
@@ -24,12 +26,12 @@ conn, addr = s_tcp.accept()
 with conn:
     print('connected by ', addr)
     while True:
-        data = conn.recv(len(str(time.time())))
+        data = conn.recv(packet_size)
         if not data:
             break
-        print("tcp:",data, "my time:",time.time(), "diff:",time.time()-float(data))
-        data = s_udp.recv(len(str(time.time())))
-        print("udp:",data, "my time:",time.time(), "diff:",time.time()-float(data))
+        print("tcp:",float(data), "my time:",time.time(), "diff:",time.time()-float(data))
+        data = s_udp.recv(packet_size)
+        print("udp:",float(data), "my time:",time.time(), "diff:",time.time()-float(data))
         
 
 
--- sender.py
+++ sender.py
@@ -2,19 +2,23 @@ import socket
 import time
 
 num_packets = 20
+packet_size = 2048
 
 s_tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s_udp = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-receiver_ip = '128.4.124.32'
+receiver_ip = '127.0.0.1'
 receiver_port_tcp = 7777
 receiver_port_udp = 7778
 
 s_tcp.connect((receiver_ip, receiver_port_tcp))
 s_udp.connect((receiver_ip, receiver_port_udp))
 for i in range(num_packets):
-    s_tcp.sendall(str(time.time()).encode('ascii'))
-    s_udp.sendall(str(time.time()).encode('ascii'))
+    cur_time = str(time.time())
+    packet = '0'*(packet_size-len(cur_time))+cur_time
+    packet = packet.encode('ascii')
+    s_tcp.sendall(packet)
+    s_udp.sendall(packet)
 
 s_tcp.close()
 s_udp.close()
